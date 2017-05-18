4d6155d7781a5ac871bf7c032db4897f6227b8d5
dr.dimitru <ceo@veliov.com>
2017-05-18 08:07:20 +0300

    v3.0.2
     - Compatibility with `meteor@1.4.4.2`
     - Dependencies update
     - Better tests
     - *Regular update*

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: 'ostrio:flow-router-title',
-  version: '3.0.1',
+  version: '3.0.2',
   summary: 'Change document.title (page title) on the fly within flow-router',
   git: 'https://github.com/VeliovGroup/Meteor-flow-router-title',
   documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.4');
-  api.use(['underscore', 'ecmascript', 'reactive-var', 'ostrio:flow-router-extra@3.1.0'], 'client');
+  api.versionsFrom('1.4');
+  api.use(['underscore', 'ecmascript', 'reactive-var', 'ostrio:flow-router-extra@3.1.1'], 'client');
   api.mainModule('flow-router-title.js', 'client');
 });
 
 Package.onTest(function(api) {
-  api.use(['tinytest', 'ecmascript', 'ostrio:flow-router-extra', 'ostrio:flow-router-title', 'random'], 'client');
+  api.use(['tinytest', 'ecmascript', 'random', 'ostrio:flow-router-extra', 'ostrio:flow-router-title'], 'client');
   api.addFiles('tests.js', 'client');
 });
