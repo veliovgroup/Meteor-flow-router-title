5d5fe27aa2c354124018e91ff62b2aab218e42c5
dr.dimitru <ceo@veliov.com>
2017-10-16 08:11:14 +0300

    v3.1.0
     - Compatibility with `meteor@1.5.2.2`
     - Fix #9 , thanks to @nicooprat
     - Make sure reactive data sources is supported and will trigger
    re-computation
     - Implement `.set()` (*see updated docs*)
     - Docs are updated
     - Dependencies update
     - More tests, 100% tests coverage, now with reactive data sources
     - Overall codebase enhancements

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:flow-router-title',
-  version: '3.0.7',
+  version: '3.1.0',
   summary: 'Change document.title (page title) on the fly within flow-router',
   git: 'https://github.com/VeliovGroup/Meteor-flow-router-title',
   documentation: 'README.md'
@@ -8,11 +8,15 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.versionsFrom('1.4');
-  api.use(['underscore', 'ecmascript', 'reactive-var', 'ostrio:flow-router-extra@3.3.1'], 'client');
+  api.use(['underscore', 'ecmascript', 'reactive-var', 'tracker', 'ostrio:flow-router-extra@3.4.0'], 'client');
   api.mainModule('flow-router-title.js', 'client');
 });
 
 Package.onTest(function(api) {
-  api.use(['tinytest', 'ecmascript', 'random', 'ostrio:flow-router-extra', 'ostrio:flow-router-title'], 'client');
-  api.addFiles('tests.js', 'client');
+  api.use(['tinytest', 'ecmascript', 'random', 'session', 'reactive-var', 'tracker', 'ostrio:flow-router-extra@3.4.0', 'ostrio:flow-router-title'], 'client');
+  api.addFiles('tests/init.js', 'client');
+  api.addFiles('tests/common.js', 'client');
+  api.addFiles('tests/group.js', 'client');
+  api.addFiles('tests/reactive.js', 'client');
+  api.addFiles('tests/group-reactive.js', 'client');
 });
